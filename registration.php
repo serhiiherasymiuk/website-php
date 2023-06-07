c184f016294c5732b16cdd387a7e42a25cb0bdd2
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-06-07 18:17:49 +0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,28 @@
+<form class="p-5 d-flex flex-column gap-3">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" aria-describedby="emailHelp"
+               placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputLastName">Last Name</label>
+        <input type="text" class="form-control" placeholder="Last Name">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPhone">Phone</label>
+        <input type="tel" class="form-control" placeholder="Phone">
+    </div>
+    <div class="mb-3">
+        <label for="formFile" class="form-label">Default file input example</label>
+        <input class="form-control" type="file">
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
